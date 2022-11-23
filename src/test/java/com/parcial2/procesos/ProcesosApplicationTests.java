cc0187b765daf71b2ee430b70c1aef21c20297fe
Pollo <jjariasa@ufpso.edu.co>
2022-11-22 20:53:15 -0500

    primer commit

---
+++ src/test/java/com/parcial2/procesos/ProcesosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parcial2.procesos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcesosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
