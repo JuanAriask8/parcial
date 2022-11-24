c295282a409abf23d55d92accb68edd0a97ab1db
Pollo <jjariasa@ufpso.edu.co>
2022-11-23 23:04:16 -0500

    Token y autenticacion de usuario avance

--- src/main/java/com/parcial2/procesos/repository/CategoriaRepository.java
+++
@@ -1,8 +0,0 @@
-package com.parcial2.procesos.repository;
-
-import com.parcial2.procesos.models.Categoria;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoriaRepository extends JpaRepository<Categoria,Long> {
-
-}
